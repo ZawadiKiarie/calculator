6ef4589e90e08a1115a293f6f664e53e5fa3a295
unknown <133970406+ZawadiKiarie@users.noreply.github.com>
2023-06-27 13:18:00 +0300

    Add basic Math funtions for calcutor
    Functions to add, subtract, multiply and divide are added.
    3 variable are created for num1, num2 and num3
    function to operate on numbers has been created

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+let num1;
+let op;
+let num2;
+
+function operate(num1, op, num2) {
+  if(op === '+') {
+    return add(num1, num2);
+  }else if(op === '-'){
+    return subtract(num1, num2);
+  }else if(op === '*'){
+    return multiply(num1, num2);
+  }else if(op === '/'){
+    return divide(num1, num2);
+  }
+}
+
+console.log(operate(1,'-',1));
